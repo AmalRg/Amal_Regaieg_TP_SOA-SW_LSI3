066d1e5b2617df929da9c18d86a41e71c4a207c0
Amal <amalregaieg42@gmail.com>
2023-11-06 23:32:19 +0100

    TP4 SOAP

--- src/serveur/ServeurJWS.java
+++ src/serveur/ServeurJWS.java
@@ -6,8 +6,10 @@ import javax.xml.ws.Endpoint;
 
 public class ServeurJWS {
     public static void main(String[] args) {
+
         CalculatriceWS calculatrice = new CalculatriceWS();
         String url = "http://localhost:8084/";
+        // Publier le service web à l'URL spécifiée.
         Endpoint.publish(url,calculatrice);
         System.out.println("Service web publié avec succès sur l'url : " + url);
     }
--- src/service/CalculatriceWS.java
+++ src/service/CalculatriceWS.java
@@ -7,6 +7,7 @@ import javax.jws.WebService;
 @WebService
 public class CalculatriceWS {
     @WebMethod(operationName = "somme")
+    // Cette méthode effectue la somme de deux nombres et renvoie le résultat.
         public double somme (@WebParam(name = "a") double a, @WebParam(name = "b") double b) {
         return a + b;
     }
