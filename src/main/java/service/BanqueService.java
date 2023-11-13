9979fe07857a95aa67a7c5e52c2e2300d786b484
Amal <amalregaieg42@gmail.com>
2023-11-14 00:04:15 +0100

    TP5_1

--- src/main/java/service/BanqueService.java
+++ src/main/java/service/BanqueService.java
@@ -1,2 +1,33 @@
-package service;public class BanqueService {
+package service;
+
+import metier.Compte;
+import javax.jws.WebMethod;
+import javax.jws.WebParam;
+import javax.jws.WebService;
+import java.util.ArrayList;
+import java.util.Date;
+
+@WebService
+public class BanqueService {
+    // Liste des comptes enregistrés
+    public ArrayList<Compte> comptes = new ArrayList<>();
+
+    // Constructeur par défaut
+    public BanqueService() {
+    }
+
+    // Méthode pour récupérer la liste des comptes
+    @WebMethod
+    public ArrayList<Compte> getComptes() {
+        comptes.add(new Compte(1, 10, new Date()));
+        comptes.add(new Compte(2, 20, new Date()));
+        comptes.add(new Compte(3, 30, new Date()));
+        return comptes;
+    }
+
+    // Méthode de conversion de montant
+    @WebMethod
+    public double conversion(@WebParam double montant) {
+        return montant * 3.3;
+    }
 }
